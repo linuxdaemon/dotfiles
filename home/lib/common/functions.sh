b1c2c380a980778f94862cdaa989105785692fd0
Aspen Barnes <aspen.barnes@datadoghq.com>
2024-07-12 15:47:20 -0400

    Add brew install script

--- home/lib/common/functions.sh
+++ home/lib/common/functions.sh
@@ -7,41 +7,20 @@ source_if_exists() {
     if [[ "$file" != /* ]]; then
         file="$HOME/$file"
     fi
+
     test -f "$file" && source "$file"
 }
 
 add_to_path() {
     if [ -d "$1" ]; then
-        export PATH="$1${PATH+:$PATH}"
+        add_to_path_always "$1"
     fi
+}
+
+add_to_path_always() {
+    export PATH="$1${PATH+:$PATH}"
 }
 
 has_cmd() {
     type "$1" &>/dev/null
-}
-
-PLATFORM="$(uname)"
-
-is_mac() {
-    [[ "$PLATFORM" = "Darwin" ]]
-}
-
-macos_pre_big_sur() {
-    local vers
-    vers="$(sw_vers -productversion)"
-    [[ "${vers%%.*}" -lt 11 ]]
-}
-
-get_link_target() {
-    if is_mac; then
-        if has_cmd greadlink; then
-            greadlink -f "$1"
-        elif macos_pre_big_sur; then
-            python3 -c 'import sys;from pathlib import Path;print(Path(sys.argv[1]).resolve())' "$1"
-        else
-            stat -f '%R' "$1"
-        fi
-    else
-        readlink -f "$1"
-    fi
 }
